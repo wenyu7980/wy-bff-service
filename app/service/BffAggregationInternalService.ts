09f604bacdf9ae7a106925c7d8859ff0514a57b2
wenyu <wenyu7980@163.com>
2021-02-28 22:00:50 +0800

    provider

--- app/service/BffAggregationInternalService.ts
+++ app/service/BffAggregationInternalService.ts
@@ -1,28 +1,47 @@
 import { Service } from 'egg';
 import { AggregationInit } from '../domain/AggregationDefine';
-import { AggregationRequirementEntity } from '../entity/AggregationEntity';
+import { AggregationProviderEntity, AggregationRequirementEntity } from '../entity/AggregationEntity';
 
 export default class BffAggregationInternalService extends Service {
   public async initAggregation(aggregation: AggregationInit) {
     const { mysql } = this.ctx.app;
-    await mysql.delete('bff_aggregation_requirement', {
-      service_name: aggregation.serviceName,
-    });
-    await mysql.delete('bff_aggregation_provider', {
-      service_name: aggregation.serviceName,
-    });
-    for (const requirement of aggregation.requirements) {
-      for (const attribute of requirement.attributes) {
-        await mysql.insert<AggregationRequirementEntity>('bff_aggregation_requirement', {
+    const conn = await mysql.beginTransaction();
+    try {
+      await mysql.delete('bff_aggregation_requirement', {
+        service_name: aggregation.serviceName,
+      });
+      await mysql.delete('bff_aggregation_provider', {
+        service_name: aggregation.serviceName,
+      });
+      // requirement
+      for (const requirement of aggregation.requirements) {
+        for (const attribute of requirement.attributes) {
+          await mysql.insert<AggregationRequirementEntity>('bff_aggregation_requirement', {
+            service_name: aggregation.serviceName,
+            class_name: attribute.className,
+            method: requirement.method,
+            path: requirement.path,
+            array_flag: attribute.arrayFlag ? 1 : 0,
+            attribute: attribute.attribute,
+            params: JSON.stringify(attribute.params),
+          });
+        }
+      }
+      // provider
+      for (const provider of aggregation.providers) {
+        await mysql.insert<AggregationProviderEntity>('bff_aggregation_provider', {
           service_name: aggregation.serviceName,
-          class_name: attribute.className,
-          method: requirement.method,
-          path: requirement.path,
-          array_flag: attribute.arrayFlag ? 1 : 0,
-          attribute: attribute.attribute,
-          params: JSON.stringify(attribute.params),
+          path: provider.path,
+          method: provider.method,
+          class_name: provider.className,
+          array_flag: provider.arrayFlag ? 1 : 0,
+          params: JSON.stringify(provider.params),
         });
       }
+      await conn.commit();
+    } catch (e) {
+      await conn.rollback();
+      throw e;
     }
   }
 }
